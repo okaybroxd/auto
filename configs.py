2ba5407789b1db679ee990d95710703b360c7b0a
xd <85302664+okaybroxd@users.noreply.github.com>
2023-10-01 19:42:40 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    API_ID = int(getenv("API_ID", "7257728"))
+    API_HASH = getenv("API_HASH", "8a1e2e70fedb8169828c46c2478374d1")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5667630550:AAG0xMjClq_3C_VImxMGLTalMwB7d_ghZnI")
+    FSUB = getenv("FSUB", "Tcnproject")
+    CHID = int(getenv("CHID", "-1001934968443"))
+    SUDO = list(map(int, getenv("SUDO").split(656059661)))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://doncana72:music1@musicbot1.nm4rmr0.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
