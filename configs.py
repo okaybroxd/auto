5f730c83e9e04707768031c334241ef2ec09c11a
xd <85302664+okaybroxd@users.noreply.github.com>
2023-10-01 20:54:22 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "7257728"))
     API_HASH = getenv("API_HASH", "8a1e2e70fedb8169828c46c2478374d1")
-    BOT_TOKEN = getenv("BOT_TOKEN", "5667630550:AAG0xMjClq_3C_VImxMGLTalMwB7d_ghZnI")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6386338280:AAFSbPAA2z1jUSPv-44Nn4QufCBcd1l6Rtc")
     FSUB = getenv("FSUB", "Tcnproject")
     CHID = int(getenv("CHID", "-1001634707201"))
     SUDO = int(getenv("SUDO", "656059661"))
