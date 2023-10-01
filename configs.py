f4dbbf33f5f9044962f71f9a1738eff4be24ff33
xd <85302664+okaybroxd@users.noreply.github.com>
2023-10-01 20:02:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = getenv("API_HASH", "8a1e2e70fedb8169828c46c2478374d1")
     BOT_TOKEN = getenv("BOT_TOKEN", "5667630550:AAG0xMjClq_3C_VImxMGLTalMwB7d_ghZnI")
     FSUB = getenv("FSUB", "Tcnproject")
-    CHID = int(getenv("CHID", "-1001934968443"))
+    CHID = int(getenv("CHID", "-1001634707201"))
     SUDO = list(map(int, getenv("SUDO", "656059661")))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://doncana72:music1@musicbot1.nm4rmr0.mongodb.net/?retryWrites=true&w=majority")
     
