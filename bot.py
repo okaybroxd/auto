8a3dcf65f027e5ba15ed8fa562562e333002a818
xd <85302664+okaybroxd@users.noreply.github.com>
2023-10-01 20:30:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,16 +14,10 @@ app = Client(
 )
 
 gif = [
-    'https://telegra.ph/file/a5a2bb456bf3eecdbbb99.mp4',
+    'https://telegra.ph/file/7f47e0f07fd2963aa3bea.mp4',
     'https://telegra.ph/file/03c6e49bea9ce6c908b87.mp4',
     'https://telegra.ph/file/9ebf412f09cd7d2ceaaef.mp4',
     'https://telegra.ph/file/293cc10710e57530404f8.mp4',
-    'https://telegra.ph/file/506898de518534ff68ba0.mp4',
-    'https://telegra.ph/file/dae0156e5f48573f016da.mp4',
-    'https://telegra.ph/file/3e2871e714f435d173b9e.mp4',
-    'https://telegra.ph/file/714982b9fedfa3b4d8d2b.mp4',
-    'https://telegra.ph/file/876edfcec678b64eac480.mp4',
-    'https://telegra.ph/file/6b1ab5aec5fa81cf40005.mp4',
     'https://telegra.ph/file/b4834b434888de522fa49.mp4'
 ]
 
@@ -38,7 +32,7 @@ async def approve(_, m : Message):
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
-        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @SdBotz__**".format(m.from_user.mention, m.chat.title))
+        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @Tcnproject**".format(m.from_user.mention, m.chat.title))
         add_user(kk.id)
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
